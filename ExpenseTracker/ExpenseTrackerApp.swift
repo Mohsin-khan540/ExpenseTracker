2379ee92f1601243a0142bc9c8f41b0fa7437e1f
Mohsin-khan540 <arhamk4555@gmail.com>
2025-12-17 21:53:25 +0500

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Mohsin khan on 17/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
